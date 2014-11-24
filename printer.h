7ce01af525c42a8d63f343b881c86bcfa64f1bd4
Brandon Bil <blbil@uwaterloo.ca>
2014-11-24 18:31:00 -0500

    Added printer impl, could use cleanup

--- printer.h
+++ printer.h
@@ -1,6 +1,8 @@
 #ifndef PRINTER_H
 #define PRINTER_H
 
+#include <map>
+
 _Cormonitor Printer {
   public:
     enum Kind { Parent, WATCardOffice, NameServer, Truck, BottlingPlant, Student, Vending, Courier };
@@ -11,6 +13,26 @@ _Cormonitor Printer {
     void print( Kind kind, unsigned int lid, char state );
     void print( Kind kind, unsigned int lid, char state, int value1 );
     void print( Kind kind, unsigned int lid, char state, int value1, int value2 );
+  private:
+    virtual void main();
+
+    struct Info
+    {
+        Kind kind;
+        char state;
+        int value1;
+        int value2;
+    };
+
+    std::map<unsigned int, Info> nonIdBuffer;   //ids -> parent = 0, watcardoffice = 1, NameServer = 2, Truck = 3, BottlingPlant = 4
+    std::map<unsigned int, Info> studentBuffer;
+    std::map<unsigned int, Info> vendingBuffer;
+    std::map<unsigned int, Info> courierBuffer;
+
+    unsigned int numStudents;
+    unsigned int numVendingMachines;
+    unsigned int numCouriers;
+
 };
 
 #endif
