3bc38b417554466bc2921f3cddbf97f0fff60925
Brandon Bil <blbil@uwaterloo.ca>
2014-11-24 13:49:46 -0500

    Added parent and bank impl

--- bank.cc
+++ bank.cc
@@ -1,8 +1,35 @@
 #include "bank.h"
 
-Bank( unsigned int numStudents ) {}
+Bank( unsigned int numStudents ) : numStudents(numStudents) {
 
-void deposit( unsigned int id, unsigned int amount ) {}
+    for(int i = 0; i < numStudents; i++){
+        accounts.insert(std::pair<unsigned int, int>(i, 0));
+        waitingOnDeposit.push_back(uCondition());
+    }
+}
 
-void withdraw( unsigned int id, unsigned int amount ) {}
+void deposit( unsigned int id, unsigned int amount ) {
+    map<unsigned int, int>::iterator it = accounts.find(id);
+    if(it != accounts.end()){
+        //account is found
+        it->second+= amount;
+
+        waitingOnDeposit[id].signal();
+    }
+}
+
+void withdraw( unsigned int id, unsigned int amount ) {
+    map<unsigned int, int>::iterator it = accounts.find(id);
+    if(it != accounts.end()){
+        //account is found
+
+        int accountBal = it->second;
+
+        while(amount > accountBal){
+            waitingOnDeposit[id].wait();
+        }
+
+        it->second-= amount;
+    }
+}
 
--- parent.h
+++ parent.h
@@ -1,8 +1,19 @@
 #ifndef PARENT_H
 #define PARENT_H
 
+#include "printer.h"
+#include "bank.h"
+#include "mprng.h"
+
+extern MPRNG MP;
+
 _Task Parent {
+  private:
     void main();
+    Printer& printer;
+    Bank& bank;
+    unsigned int numStudents;
+    unsigned int parentalDelay;
   public:
     Parent( Printer &prt, Bank &bank, unsigned int numStudents, unsigned int parentalDelay );
 };
