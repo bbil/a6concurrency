516e9a95205764170ba217541979c121acdf5a8c
Brandon Bil <blbil@uwaterloo.ca>
2014-11-25 18:50:56 -0500

    oNameServer IMPl

--- nameserver.cc
+++ nameserver.cc
@@ -2,13 +2,56 @@
 #include "printer.h"
 #include "vendingmachine.h"
 
-NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents ) {}
+NameServer::NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents ) : printer(prt), numVendingMachines(numVendingMachines), numStudents(numStudents) {
+    
+    //initialize map that tells a student which vending machine to go to
+    for(unsigned int i=0; i < numStudents; i++){
+        unsigned int vendingMachineIdx = i % numVendingMachines;
 
-void NameServer::main() {}
+        studentToVending.insert(std::pair<unsigned int, unsigned int>(i, vendingMachineIdx));
+    }
+}
 
-void NameServer::VMregister( VendingMachine *vendingmachine ) {}
+void NameServer::VMregister( VendingMachine *vendingmachine ) {
+    unsigned int id = vendingmachine->getId();
 
-VendingMachine *NameServer::getMachine( unsigned int id ) {}
+    vendingMachines.insert(std::pair<unsigned int, VendingMachine*>(id, vendingmachine));
+}
 
-VendingMachine **NameServer::getMachineList() {}
+VendingMachine *NameServer::getMachine( unsigned int id ) {
+    VendingMachine* vm = NULL;
+
+    unsigned int vendingMachineIdx = studentToVending.at(id);
+
+    if(vendingMachines.find(vendingMachineIdx) != vendingMachines.end()){
+        vm = vendingMachines.at(vendingMachineIdx);
+
+
+        //give the student the next vending machine id (modulo), for the next time getMachine is called by the student
+        unsigned int newIdx = (vendingMachineIdx + 1) % numVendingMachines;
+        studentToVending[id] = newIdx;
+    }
+
+    return vm;
+}
+
+VendingMachine **NameServer::getMachineList() {
+
+    VendingMachine** machines = new VendingMachine*[numVendingMachines];
+
+    for(unsigned int i=0; i < numVendingMachines; i++){
+        machines[i] = vendingMachines.at(i);
+    }
+
+    return machines;
+}
+
+void NameServer::main() {
+    for(;;){
+        _Accept(~VendingMachine);
+        or _Accept(getMachine);
+        or _Accept(getMachineList);
+        or _Accept(VMregister);
+    }
+}
 
--- nameserver.h
+++ nameserver.h
@@ -1,10 +1,22 @@
 #ifndef NAMESERVER_H
 #define NAMESERVER_H
 
+#include <map>
+
 _Task VendingMachine;
 _Cormonitor Printer;
 
 _Task NameServer {
+  private:
+
+    std::map<unsigned int, unsigned int> studentToVending;  //holds the vending machine id that a student is currently located at
+    std::map<unsigned int, VendingMachine*> vendingMachines; //holds a relationship to a vending machine to its id
+    
+
+    Printer& printer;
+    unsigned int numVendingMachines;
+    unsigned int numStudents;
+
     void main();
   public:
     NameServer( Printer &prt, unsigned int numVendingMachines, unsigned int numStudents );
