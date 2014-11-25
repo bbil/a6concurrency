96acf87a9e3629b28d56e610a0fac8eca16cd038
Brandon Bil <blbil@uwaterloo.ca>
2014-11-25 14:42:04 -0500

    start bottlingplant

--- bottlingplant.cc
+++ bottlingplant.cc
@@ -1,11 +1,20 @@
 #include "bottlingplant.h"
 #include "printer.h"
 #include "nameserver.h"
+#include "truck.h"
 
 BottlingPlant::BottlingPlant( Printer &prt, NameServer &nameServer, unsigned int numVendingMachines, unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour, 
-                            unsigned int timeBetweenShipments ) {}
+                            unsigned int timeBetweenShipments ) : printer(prt), nameServer(nameServer), numVendingMachines(numVendingMachines), maxShippedPerFlavour(maxShippedPerFlavour),
+                            maxStockPerFlavour(maxStockPerFlavour), timeBetweenShipments(timeBetweenShipments) {}
 
-void BottlingPlant::main() {}
+BottlingPlant::~BottlingPlant(){
+    delete truck;
+}
+
+void BottlingPlant::main() {
+    //create truck
+    truck = new Truck(printer, nameServer, *this, numVendingMachines, maxStockPerFlavour);
+}
 
 void BottlingPlant::getShipment( unsigned int cargo[] ) {}
 
--- bottlingplant.h
+++ bottlingplant.h
@@ -3,14 +3,27 @@
 
 _Cormonitor Printer;
 _Task NameServer;
+#include "truck.h"
+#include "mprng.h"
+
+extern MPRNG MP;
 
 _Task BottlingPlant {
+  private:
+    Printer& printer;
+    NameServer& nameServer;
+    Truck* truck;
+    unsigned int numVendingMachines;
+    unsigned int maxShippedPerFlavour;
+    unsigned int maxStockPerFlavour;
+    unsigned int timeBetweenShipments;
     void main();
   public:
     _Event Shutdown {};                    // shutdown plant
     BottlingPlant( Printer &prt, NameServer &nameServer, unsigned int numVendingMachines,
                  unsigned int maxShippedPerFlavour, unsigned int maxStockPerFlavour,
                  unsigned int timeBetweenShipments );
+    ~BottlingPlant();
     void getShipment( unsigned int cargo[] );
 };
 
